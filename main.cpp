4bcb443120a578136affba4cc426f6087abde1b1
KALLAYAN <kallayandatta70@gmail.com>
2025-01-10 16:16:06 +0600

    New Code added in c ++

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main() {
     cout<<"Hello world!"<<endl;
+    int a;
+    cout<<"Enter a number : " << endl;
+    cin>> a;
+    cout<<"You entered "<<a<<endl;
     return 0;
 }
