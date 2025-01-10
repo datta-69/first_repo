da369dc59ce44e535074b3138eb97a6b417a8c0c
KALLAYAN <kallayandatta70@gmail.com>
2025-01-10 16:54:32 +0600

    Calclulate sum and multiplication

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 int main() {
     cout<<"Hello world!"<<endl;
-    int a;
-    cout<<"Enter a number : " << endl;
+    int a,b;
+
+    cout<<"Enter first number : " << endl;
     cin>> a;
-    cout<<"You entered "<<a<<endl;
+
+    cout<<"Enter second number : "<< endl;
+    cin>>b;
+
+    cout<<"You entered "<<a<< "and" <<b <<endl;
+    cout<< "The sum of this two number is : "<<a+b<<endl;
+
+    cout<<"The multiplication of this two number is : "<< a*b <<endl;
+    
     return 0;
 }
