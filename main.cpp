ef6c76626bbf144a6e0f677fa058925b0b018406
KALLAYAN <kallayandatta70@gmail.com>
2025-01-10 17:07:42 +0600

    difference and division added

--- main.cpp
+++ main.cpp
@@ -14,6 +14,16 @@ int main() {
     cout<< "The sum of this two number is : "<<a+b<<endl;
 
     cout<<"The multiplication of this two number is : "<< a*b <<endl;
-    
+    if (a>b)
+    {
+        cout<<"The difference of this two number is : "<< a-b <<endl;
+    }
+    else {
+     cout<<"The difference of this two number is : "<< b-a <<endl;
+    }
+    float c = a+0.00;
+    float d = b+ 0.00;
+    cout<< "The division of a/b is : "<< c/d << endl;
+    cout<< "The division of b/a is : "<< d/c <<endl;
     return 0;
 }
